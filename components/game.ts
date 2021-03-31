c4a153f4c44a996eb12abf8d0cab0d8caf7a84d7
Chandler Talbot <crtalbot98@gmail.com>
2021-03-30 21:23:42 -0400

    added objects to game scene

--- components/game.ts
+++ components/game.ts
@@ -1,6 +1,7 @@
 import ObjectHandler from "./object-handler.js";
 import {Controller} from "./controller.js";
 import Player from "./player.js";
+import GameBorder from "./game-borders.js";
 import {controlPressed} from "../helpers.js";
 
 export default class Game{
@@ -10,27 +11,35 @@ export default class Game{
     public ctx: CanvasRenderingContext2D;
     protected objectHandler: ObjectHandler;
     protected player: Player;
+    protected gameBorder: GameBorder;
 
     constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
         this.c = canvas;
         this.ctx = ctx;
-        this.objectHandler = new ObjectHandler(this.ctx);
+        this.objectHandler = new ObjectHandler(this.ctx, this.c);
         this.player = new Player(this.c);
+        this.gameBorder = new GameBorder(this.ctx, this.c);
+        window.setInterval(() => {
+            this.objectHandler.addToObjects()
+        }, 1200)
     }
 
-    protected update(frame: number): void{
+    protected update(frame: number): void{ // Updates data for objects and scene
         this.player.updatePos();
+        this.objectHandler.updateByFrame()
     }
 
-    protected draw(): void{
+    protected draw(): void{ // Draws the scene every frame
         this.ctx.clearRect(this.c.offsetWidth * 0.2, 0, this.c.offsetWidth*0.6, this.c.height);
-        this.player.create(this.ctx)
-        // for(let i = 0; i < this.objectHandler.getObject().length; i++){
-        //     //Draw objects
-        // }
+
+        this.gameBorder.create(); // border for the game area
+
+        this.player.create(this.ctx); // player
+
+        this.objectHandler.createByFrame() // All objects in object array
     }
 
-    public loop(): void{
+    public loop(): void{ // Creates the animation loop
         const animate = (): void => {
             window.requestAnimationFrame(animate);
             this.update(this.time);
@@ -40,7 +49,7 @@ export default class Game{
         animate();
     }
 
-    public initControls(): void{
+    public initControls(): void{ // Creates the player controls
         document.addEventListener('keydown',(e) => {
             if(Controller[e.keyCode]){
                 if(!controlPressed()){
