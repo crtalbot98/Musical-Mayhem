fee1fff1858273f364ec99cc7d2b4f4361cd1288
Chandler Talbot <crtalbot98@gmail.com>
2021-03-30 21:25:08 -0400

    init commit for game border

---
+++ components/game-borders.ts
@@ -0,0 +1,16 @@
+export default class GameBorder{
+
+    private ctx: CanvasRenderingContext2D;
+    private c: HTMLCanvasElement;
+
+    constructor(ctx: CanvasRenderingContext2D, c: HTMLCanvasElement) {
+        this.ctx = ctx;
+        this.c = c
+    }
+
+    public create(): void{
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(0,0,this.c.offsetWidth * 0.2, this.c.height);
+        this.ctx.fillRect(this.c.offsetWidth-this.c.offsetWidth * 0.2,0,this.c.offsetWidth * 0.2, this.c.height);
+    }
+}
