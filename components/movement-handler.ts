4044d132d15842f466217f262fc9a2f09bd842dc
Chandler Talbot <crtalbot98@gmail.com>
2021-03-30 21:25:50 -0400

    updated to take different sizes

--- components/movement-handler.ts
+++ components/movement-handler.ts
@@ -1,27 +1,31 @@
 import {Coords, Size} from "./types";
 
-export default class MovementHandler{//Object on the left move up and right move down
+export default class MovementHandler{// Handles the movement of objects on the x and y axis
 
     protected pos: Coords;
+    protected c: HTMLCanvasElement;
+    protected size: Size;
 
     constructor(c: HTMLCanvasElement, size: Size) {
-        this.pos = this.generateRandomSide(c, size);
+        this.c = c;
+        this.size = size;
+        this.pos = this.generateRandomSide()
     }
 
-    updateYPos(c: HTMLCanvasElement, size: Size): void{
-        if(this.pos.x >= c.offsetWidth/2 - size.w/2){
+    updateYPos(): void{ // Move an object; left of center moves down and right of center moves up
+        if(this.pos.x >= this.c.offsetWidth/2 - this.size.w/2){
             this.pos.y -= 2;
         }
-        else if(this.pos.x <= c.offsetWidth/2 + size.w/2){
+        else if(this.pos.x <= this.c.offsetWidth/2 + this.size.w/2){
             this.pos.y += 2;
         }
     }
 
-    public updateXPos(c: HTMLCanvasElement, size: Size): void{
-        if(this.pos.side === 'left' && this.pos.x >= c.offsetWidth*0.2){
+    public updateXPos(): void{ // Move and object; can on move back to the other side of past other center
+        if(this.pos.side === 'left' && this.pos.x >= this.c.offsetWidth*0.2){
             this.pos.x -= 4
         }
-        else if(this.pos.side === 'right' && this.pos.x <= c.offsetWidth - c.offsetWidth*0.2 - size.w){
+        else if(this.pos.side === 'right' && this.pos.x <= this.c.offsetWidth - this.c.offsetWidth*0.2 - this.size.w){
             this.pos.x += 4;
         }
     }
@@ -30,23 +34,27 @@ export default class MovementHandler{//Object on the left move up and right move
         this.pos.side = side;
     }
 
-    private generateRandomSide(c: HTMLCanvasElement, size: Size): Coords{ // update y value
+    private generateRandomSide(): Coords{ // randomly choose the side and position an object will spawn
         const r = Math.random();
-        if(r > 0.5){
+        if(r >= 0.5){
             return{
-                x: c.width * 0.2,
-                y: 25,
+                x: this.c.width * 0.2,
+                y: this.size.h * 2,
                 side: 'left'
             };
         }
         return {
-            x: c.offsetWidth-c.offsetWidth * 0.2 - c.offsetWidth/8,
-            y: c.offsetHeight - size.w - 25,
+            x: this.c.offsetWidth - this.c.offsetWidth * 0.2 - this.size.w,
+            y: this.c.offsetHeight - this.size.w - this.size.h * 2,
             side: 'right'
         }
     }
 
-    public getPos(): Coords{
+    public resetPos(){ // Resets the position of an object back to the random position
+        this.pos = this.generateRandomSide()
+    }
+
+    public getPos(): Coords{ // Return the position of the object
         return this.pos
     }
 }
