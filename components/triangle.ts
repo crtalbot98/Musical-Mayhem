79486c32ecc5c91ff42d9644ec5825a5f4335ab5
Chandler Talbot <crtalbot98@gmail.com>
2021-05-02 16:53:31 -0400

    added bounds for positioning

--- components/triangle.ts
+++ components/triangle.ts
@@ -6,21 +6,33 @@ export default class Triangle extends Obstacle{
         super(c, {w: 50, h: 50});
     }
 
-    create(ctx: CanvasRenderingContext2D){ // Draws a triangle to the scene
-        if(this.movementHandler.getPos().side === 'left'){
-            ctx.beginPath();
-            ctx.moveTo(this.movementHandler.getPos().x, this.movementHandler.getPos().y);
-            ctx.lineTo(this.movementHandler.getPos().x + this.size.w, this.movementHandler.getPos().y - this.size.h);
-            ctx.lineTo(this.movementHandler.getPos().x, this.movementHandler.getPos().y - this.size.h*2);
-            ctx.fill();
+    create(ctx: CanvasRenderingContext2D): void{ // Draws a triangle to the scene
+        let bounds = this.movementHandler.getBounds();
+        bounds.p2 = {x: bounds.p1.x + this.size.w, y: bounds.p1.y - this.size.h};
+
+        ctx.beginPath();
+        ctx.fillStyle = '#000000';
+        if(this.movementHandler.getSide() === 'left'){
+            // Create bounds for left-side triangle
+            bounds.p3 = {x: bounds.p1.x, y: bounds.p1.y - this.size.h*2};
+            this.movementHandler.updateBounds(bounds);
+
+            // Draw triangle to canvas
+            ctx.moveTo(bounds.p1.x, bounds.p2.y);
+            ctx.lineTo(bounds.p2.x, bounds.p2.y);
+            ctx.lineTo(bounds.p3.x, bounds.p3.y);
         }
-        else if(this.movementHandler.getPos().side === 'right'){
-            ctx.beginPath();
-            ctx.moveTo(this.movementHandler.getPos().x, this.movementHandler.getPos().y);
-            ctx.lineTo(this.movementHandler.getPos().x + this.size.w, this.movementHandler.getPos().y - this.size.h);
-            ctx.lineTo(this.movementHandler.getPos().x + this.size.w, this.movementHandler.getPos().y + this.size.h);
-            ctx.fill();
+        else if(this.movementHandler.getSide() === 'right'){
+            // Create bounds for right-side triangle
+            bounds.p3 = {x: bounds.p1.x + this.size.w, y: bounds.p1.y + this.size.h};
+            this.movementHandler.updateBounds(bounds);
+
+            // Draw triangle to canvas
+            ctx.moveTo(bounds.p1.x, bounds.p1.y);
+            ctx.lineTo(bounds.p2.x, bounds.p2.y);
+            ctx.lineTo(bounds.p3.x, bounds.p3.y);
         }
+        ctx.fill();
     }
 
     public getType(): string{ // Returns the objects type
