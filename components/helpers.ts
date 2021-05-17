c015d91f6efddb275b624c4ce92b479e465b4e32
Chandler Talbot <crtalbot98@gmail.com>
2021-05-17 19:54:59 -0400

    added fetch requests for spotify api

--- components/helpers.ts
+++ components/helpers.ts
@@ -1,5 +1,5 @@
-import {Bounds, Size} from "./types.js";
-import {Controller} from "./controller.js";
+import {Bounds, Params, Size} from "./game/types.js";
+import {Controller} from "./game/controller.js";
 
 export function checkSide(pos: Bounds, size: Size, c: HTMLCanvasElement): string{
         if(pos.tr.x <= c.offsetWidth/2) return 'left';
@@ -10,4 +10,24 @@ export function controlPressed(): boolean{
         for(let control in Controller){
                 if(Controller[control].pressed) return true
         }
+}
+
+export async function fetchSpotifyAPI(url: string, auth: string){
+        let data = await fetch(url, {
+                method: 'get',
+                headers: new Headers({
+                        'Authorization': `Bearer ${auth}`
+                })
+        });
+
+        try{
+                return await data.json()
+        }
+        catch(e){
+                return 'Error: unable to connect'
+        }
+}
+
+export function checkParams(params: Params): boolean{
+        return (params !== undefined && 'access_token' in params && 'refresh_token' in params)
 }
