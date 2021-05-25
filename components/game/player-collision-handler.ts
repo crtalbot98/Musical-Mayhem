72634587a2729b9e5211eb2670540db7eeb17ed9
Chandler Talbot <crtalbot98@gmail.com>
2021-05-25 14:02:17 -0400

    overhauled collision detection to take edges into account

--- components/game/player-collision-handler.ts
+++ components/game/player-collision-handler.ts
@@ -1,5 +1,5 @@
 import CollisionHandler from "./collision-handler.js";
-import {Bounds, Bound, Size} from "./types";
+import {Bounds, Size, Bound} from "../types";
 
 class PlayerCollisionHandler extends CollisionHandler{
 
@@ -10,19 +10,37 @@ class PlayerCollisionHandler extends CollisionHandler{
         this.size = size
     }
 
-    public withinPlayer(obstacles: any[], bounds: Bounds): boolean{ // Check if obstacles are within the player rect
-        if(!bounds.tr || !bounds.bl || !bounds.br) return false;
+    public withinPlayer(obstacles: any[], pBounds: Bounds, playerSide: string): boolean{ // Check if obstacles are within the player rect
+        if(!pBounds.tr || !pBounds.bl || !pBounds.br) return false;
         for(let i = 0; i < obstacles.length; i++){
-            const obsBounds = obstacles[i].movementHandler.getBounds();
-            for(let obs in obsBounds){
-                const tlb = bounds.p1.x <= obsBounds[obs].x && bounds.p1.y <= obsBounds[obs].y;
-                const trb = bounds.tr.x >= obsBounds[obs].x && bounds.tr.y <= obsBounds[obs].y;
-                const blb = bounds.bl.x <= obsBounds[obs].x && bounds.bl.y >= obsBounds[obs].y;
-                const brb = bounds.br.x >= obsBounds[obs].x && bounds.br.y >= obsBounds[obs].y;
-                if(tlb && trb && blb && brb) return true
+            const oBounds = obstacles[i].movementHandler.getBounds();
+            if(playerSide === obstacles[i].movementHandler.getSide()){
+                for(let p in oBounds) {
+                    const ab = this.vector(pBounds.br, pBounds.bl);
+                    const am = this.vector(pBounds.br, oBounds[p]);
+                    const bc = this.vector(pBounds.bl, pBounds.p1);
+                    const bm = this.vector(pBounds.bl, oBounds[p]);
+                    const dotABAM = this.dot(ab, am);
+                    const dotABAB = this.dot(ab, ab);
+                    const dotBCBM = this.dot(bc, bm);
+                    const dotBCBC = this.dot(bc, bc);
+                    if(0 <= dotABAM && dotABAM <= dotABAB && 0 <= dotBCBM && dotBCBM <= dotBCBC)  return true
+                }
+
+                return false
             }
-            return false
         }
+    }
+
+    private vector(p1: Bound, p2: Bound): Bound{
+        return {
+            x: (p2.x - p1.x),
+            y: (p2.y - p1.y)
+        }
+    }
+
+    private dot(u: Bound, v: Bound): number{
+        return u.x * v.x + u.y * v.y
     }
 }
 
