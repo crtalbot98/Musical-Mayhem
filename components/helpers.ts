07f83ac33960441aae2910bf1fcf61114b0ed1ad
Chandler Talbot <crtalbot98@gmail.com>
2021-05-25 14:09:10 -0400

    added randomBetweenToVal and updateVisibility to helpers

--- components/helpers.ts
+++ components/helpers.ts
@@ -1,4 +1,4 @@
-import {Bounds, Params, Size} from "./game/types.js";
+import {Bounds, Params, Size} from "./types.js";
 import {Controller} from "./game/controller.js";
 
 export function checkSide(pos: Bounds, size: Size, c: HTMLCanvasElement): string{
@@ -30,4 +30,21 @@ export async function fetchSpotifyAPI(url: string, auth: string){
 
 export function checkParams(params: Params): boolean{
         return (params !== undefined && 'access_token' in params && 'refresh_token' in params)
+}
+
+export function randBetweenTwoVal(min: number, max: number): number{
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+}
+
+export function updateVisibility(): void{
+        const displayNone = Array.from(document.querySelectorAll('.display-none'));
+        const flex = Array.from(document.querySelectorAll('.flex'));
+        const elements = displayNone.concat(flex);
+
+        for(let i = 0; i < elements.length; i++){
+                if(elements[i].classList.contains('display-none')) elements[i].classList.replace('display-none', 'flex');
+                else if(elements[i].classList.contains('flex')) elements[i].classList.replace('flex', 'display-none');
+        }
 }
