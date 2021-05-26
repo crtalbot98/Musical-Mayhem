8ac6742b43de87768f1547ff4cf6bb4ee50c9283
Chandler Talbot <crtalbot98@gmail.com>
2021-05-25 22:23:34 -0400

    added state handling for play button

--- components/spotify/spotify-player.ts
+++ components/spotify/spotify-player.ts
@@ -1,5 +1,5 @@
 import StateHandler from "../state-handler.js";
-import {fetchSpotifyAPI, updateVisibility} from "../helpers.js";
+import {fetchSpotifyAPI, updatePlayBtn, updateVisibility} from "../helpers.js";
 
 export default class SpotifyPlayer{
 
@@ -18,7 +18,7 @@ export default class SpotifyPlayer{
     public initSpotifyPlayer(): void{
         if(!this._params.access_token) return;
 
-        const player = new Spotify.Player({
+        this._player = new Spotify.Player({
             name: 'Beats Per Jump Player',
             getOAuthToken: (cb: any) => {
                 cb(this._params.access_token);
@@ -26,30 +26,29 @@ export default class SpotifyPlayer{
         });
 
         // Error handling
-        player.addListener('initialization_error', ({message}: any) => { this.reset() });
-        player.addListener('authentication_error', ({message}: any) => { this.reset() });
-        player.addListener('account_error', ({message}: any) => { this.reset() });
-        player.addListener('playback_error', ({message}: any) => { this.reset() });
+        this._player.addListener('initialization_error', ({message}: any) => { this.reset() });
+        this._player.addListener('authentication_error', ({message}: any) => { this.reset() });
+        this._player.addListener('account_error', ({message}: any) => { this.reset() });
+        this._player.addListener('playback_error', ({message}: any) => { this.reset() });
 
         // Playback status updates
-        player.addListener('player_state_changed', (state: any) => {
+        this._player.addListener('player_state_changed', (state: any) => {
             // if(state) console.log(state)
         });
 
         // Ready
-        player.addListener('ready', ({device_id}: any) => {
-            this._player = player;
+        this._player.addListener('ready', ({device_id}: any) => {
             this._id = device_id;
             this._ready = true
         });
 
         // Not Ready
-        player.addListener('not_ready', ({device_id}: any) => {
+        this._player.addListener('not_ready', ({device_id}: any) => {
             this.reset()
         });
 
         // Connect to the player
-        player.connect()
+        this._player.connect()
     }
 
     public playSong(uri: string, id: string): void{
@@ -66,6 +65,7 @@ export default class SpotifyPlayer{
                 this._stateHandler.toggle();
             }
             else if(data.ok){
+                updatePlayBtn(this._stateHandler.state);
                 fetchSpotifyAPI(`https://api.spotify.com/v1/audio-analysis/${id}`, this._params.access_token).then((analysis) => {
                     this._stateHandler.difficulty = analysis.track.tempo
                 })
@@ -75,13 +75,15 @@ export default class SpotifyPlayer{
 
     public pauseSong(): void{
         this._player.pause().then(() => {
-            this._stateHandler.toggle()
+            this._stateHandler.toggle();
+            updatePlayBtn(this._stateHandler.state);
         });
     }
 
     public resumeSong(): void{
         this._player.resume().then(() => {
-            this._stateHandler.toggle()
+            this._stateHandler.toggle();
+            updatePlayBtn(this._stateHandler.state);
         });
     }
 
