ab8ad7f2fd82332f6cc0c5ec7f72e00e2950620a
Chandler Talbot <crtalbot98@gmail.com>
2021-05-25 22:20:56 -0400

    refactored state handling

--- components/spotify/play-btn.ts
+++ components/spotify/play-btn.ts
@@ -1,24 +1,27 @@
 import StateHandler from "../state-handler.js";
 import SpotifyPlayer from "./spotify-player.js";
+import {updatePlayBtn} from "../helpers.js";
 
 export default class PlayBtn{
 
     private _stateHandler: StateHandler;
     private _player: SpotifyPlayer;
+    private _btn: HTMLButtonElement;
 
     constructor(state: StateHandler, player: SpotifyPlayer) {
         this._stateHandler = state;
-        this._player = player
+        this._player = player;
+        this._btn = document.createElement('button');
     }
 
     public create(): void{
-        const btn = document.createElement('button');
-        btn.classList.add('btn-link');
+        this._btn.id = 'play-btn';
+        this._btn.classList.add('btn-link', 'circle', 'center');
+        updatePlayBtn(this._stateHandler.state);
 
-        document.querySelector('.content').append(btn);
+        document.querySelector('.content').append(this._btn);
 
-        btn.addEventListener('click', () => {
-            btn.textContent = this._stateHandler.state ? 'Play' : 'Paused';
+        this._btn.addEventListener('click', () => {
             this._stateHandler.state ? this._player.pauseSong() : this._player.resumeSong()
         })
     }
