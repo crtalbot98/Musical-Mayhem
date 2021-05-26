a72bad29b25568e651ed6767eed323d75613d0c5
Chandler Talbot <crtalbot98@gmail.com>
2021-05-25 22:24:00 -0400

    support for open button

--- components/spotify/spotify-ui.ts
+++ components/spotify/spotify-ui.ts
@@ -3,6 +3,7 @@ import StateHandler from "../state-handler.js";
 import SpotifyPlayer from "./spotify-player.js";
 import Playlists from "./playlists.js";
 import PlayBtn from "./play-btn.js";
+import OpenBtn from "./open-btn.js";
 
 export default class SpotifyUI{
 
@@ -11,13 +12,15 @@ export default class SpotifyUI{
     private _playlists: Playlists;
     private _params: Params;
     private _playBtn: PlayBtn;
+    private _openBtn: OpenBtn;
 
     constructor(params: Params, state: StateHandler) {
         this._params = params;
         this._stateHandler = state;
         this._player = new SpotifyPlayer(params, state);
-        this._playlists = new Playlists(params, this._player);
-        this._playBtn = new PlayBtn(state, this._player)
+        this._playlists = new Playlists(params, this._player, state);
+        this._playBtn = new PlayBtn(state, this._player);
+        this._openBtn = new OpenBtn(state)
     }
 
     public loginWarning(): void{
@@ -28,8 +31,9 @@ export default class SpotifyUI{
     }
 
     public init(): void{
-        this._playlists.updatePlaylists();
-        this._playBtn.create()
+        this._playlists.update();
+        this._playBtn.create();
+        // this._openBtn.init()
     }
 
     public get player(): SpotifyPlayer{
