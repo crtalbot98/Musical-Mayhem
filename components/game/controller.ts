64ab10268750267039eae220a0a38561ffebfc61
Chandler Talbot <crtalbot98@gmail.com>
2021-05-25 22:13:44 -0400

    refactored side check

--- components/game/controller.ts
+++ components/game/controller.ts
@@ -13,9 +13,9 @@ export const Controller: PlayerController = {
 };
 
 function moveLeft(player: any, c: HTMLCanvasElement): void{
-    if(checkSide(player.movementHandler.bounds, player.size, c) === 'right') player.movementHandler.updateSide('left')
+    if(checkSide(player._movementHandler.bounds, player.size, c) === 'right') player._movementHandler.side = 'left'
 }
 
 function moveRight(player: any, c: HTMLCanvasElement): void{
-    if(checkSide(player.movementHandler.bounds, player.size, c) === 'left') player.movementHandler.updateSide('right')
+    if(checkSide(player._movementHandler.bounds, player.size, c) === 'left') player._movementHandler.side = 'right'
 }
